47cae279b6bc20addda71d1e031138ffc2ad1f43
Phagun Baya <phagun.baya@gmail.com>
2016-03-03 07:46:45 -0800

    test for output

---
+++ test/TestPipelineGetOutput.js
@@ -0,0 +1,59 @@
+/*!
+ * falkonry-js-client
+ * Copyright(c) 2016 Falkonry Inc
+ * MIT Licensed
+ */
+
+'use strict';
+
+/**
+ * Module dependencies
+ */
+
+var fs       = require('fs');
+var assert   = require('assert');
+var Falkonry = require('../').Client;
+var token    = 'dv762fciuqmj6p'; //auth token
+var pipeline = ''; //Pipeline id
+
+/*
+ * Test to get output of a Pipeline
+ * and pipe it to a writable file stream
+ */
+
+describe('Test get output of a Pipeline', function(){
+  var falkonry = null;
+
+  before(function(done){
+    falkonry = new Falkonry('http://localhost:8080', token);
+    return done();
+  });
+
+  it('Should get complete output', function(done){
+    return falkonry.getOutput(pipeline, null, null, function(error, response){
+      assert.equal(error, null, 'Error getting output of a Pipeline: '+error);
+
+      if(!error) {
+        response.pipe(fs.createWriteStream('/tmp/'+pipeline+'.json'));
+      }
+      return done();
+    });
+  });
+
+  it.skip('Should get output within the specified range', function(done){
+    var start = 0;
+    var end   = 0;
+    return falkonry.getOutput(pipeline, start, end, function(error, response){
+      assert.equal(error, null, 'Error getting output of a Pipeline: '+error);
+
+      if(!error) {
+        response.pipe(fs.createWriteStream('/tmp/'+pipeline+'.json'));
+      }
+      return done();
+    });
+  });
+
+  after(function(done){
+    return done();
+  });
+});
