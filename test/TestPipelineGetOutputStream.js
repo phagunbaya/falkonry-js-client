aadfb2133c6021a4a07f3ee4be5eda7c8c835fea
SwapneelM <swapneel.mehta@gmail.com>
2016-08-05 13:42:31 +0530

    Error detection for pipeline closed

--- test/TestPipelineGetOutputStream.js
+++ test/TestPipelineGetOutputStream.js
@@ -14,9 +14,9 @@ var fs       = require('fs');
 var assert   = require('assert');
 var Falkonry = require('../').Client;
 var Events   = require('events');
-var host     = 'http://192.168.1.202:8080';
-var token    = 'wluja163da0f8a3451mhyyqrtsuclvb7';
-var pipeline = 'nzqnyhqcgdx8dx'; //Pipeline id
+var host     = 'http://dev.falkonry.io';
+var token    = 'dg5th0r2rj4rywg3rv90egu3krswpw50';
+var pipeline = 'tu4tfecsafdoi1'; //Pipeline id
 var updateNotifier;
 var runnerID;
 
@@ -47,6 +47,10 @@ describe('Test get output of a Pipeline', function(){
                 updateNotifier.on('data', function (data) {
                     console.log(data);
                 });
+                updateNotifier.on('error', function (error) {
+                    console.log("Error : " + error);
+                    return done;
+                })
             }
         });
     });
@@ -54,34 +58,8 @@ describe('Test get output of a Pipeline', function(){
     it('Should end output stream', function(done){
         this.timeout(3600000);
         setTimeout(done, 3600000);
-
-        var start = "1467729648422";
-        return falkonry.streamOutput(pipeline, start, function (error) {
-            assert.equal(error, null, 'Unable to stop process');
-
-            if (!error) {
-                return done();
-            }
-        });
+        return updateNotifier.close();
     });
-    /*it('Should get output within the specified range', function(done){
-        this.timeout(4000);
-        setTimeout(done, 4000);
-        //setTimeout();
-        var start = 0;
-        //var end   = 0;
-        return falkonry.endOutputStream(pipeline, function(error, response){
-            assert.equal(error, null, 'Error getting output of a Pipeline: '+error);
-
-            if(!error) {
-                //response.pipe(fs.createWriteStream('/tmp/'+pipeline+'.json'));
-                //response.on('end', function(){
-                //});
-
-                return done();
-            }
-        });
-    });*/
 
     after(function(done){
         return done();
